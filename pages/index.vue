ddc725495aa8fe4781fbad8f415c10b880b0dc9b
kongchuishan <cshan.kong@gmail.com>
2018-12-17 15:02:28 +0800

    feat: 完成首页

--- pages/index.vue
+++ pages/index.vue
@@ -1,44 +1,32 @@
 <template>
-  <section class="container">
-    1
-  </section>
+  <div class="page-index">
+    <el-row>
+      <el-col :span="5">
+        <emenu/>
+      </el-col>
+      <el-col :span="19">
+        <life/>
+      </el-col>
+    </el-row>
+    <el-row :span="24">
+      <!-- <artistic/> -->
+    </el-row>
+  </div>
 </template>
 
 <script>
-
+import Emenu from '@/components/index/menu.vue'
+import Life from '@/components/index/life.vue'
+import Artistic from '@/components/index/artistic.vue'
 export default {
+  components: {
+    Emenu,
+    Life,
+    Artistic
+  }
 }
 </script>
 
-<style>
-
-.container {
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
+<style lang="scss">
+  @import "@/assets/css/index/index.scss"
 </style>
